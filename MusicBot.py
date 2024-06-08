a4abc4f10bfe56a971f4b083b1d344916a2642d2
EnriqueThomazz <enriquethomaz18@gmail.com>
2024-06-08 12:58:43 -0300

    Atualizando código do bot de Musica pra nova versao do discor

--- MusicBot.py
+++ MusicBot.py
@@ -13,16 +13,18 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print('Logado como {0.user}'.format(client))
     updateQueue.start()
+    timeoutLeave.start()
 
 musicQueue = []
-musicChannel = []
+musicChannel = None
+timedOut = False
 
 async def nextSong():
     global musicQueue, musicChannel
 
     ctx = musicChannel    
 
-    if ctx != [] and musicQueue != []:
+    if ctx != None and musicQueue != []:
         voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         if not voice.is_playing():
 
@@ -31,7 +33,7 @@ async def nextSong():
             embed = discord.Embed(
                 colour=discord.Colour.green(),
                 title = "Atualmente tocando:",
-                description = music[1],
+                description = f"{music[1]}\n\nCortesia de <@{music[3]}>",
             )
             
             embed.set_thumbnail(url=music[2])
@@ -46,7 +48,7 @@ async def nextSong():
 #Plays music
 @client.command()
 async def play(ctx, *, Music):
-    global musicQueue, musicChannel
+    global musicQueue, musicChannel, timedOut
 
     #Connecting to a voice channel
     if ctx.voice_client == None:
@@ -75,14 +77,15 @@ async def play(ctx, *, Music):
 
     #Actually playing or adding to queue
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    musicQueue.append([data[0], data[1], data[2]])
+    musicQueue.append([data[0], data[1], data[2], ctx.author.id])
+    timedOut = False
     if not voice.is_playing():
         musicChannel = ctx
         await nextSong()
     else:
         embed = discord.Embed(
-            title = "Música adicionada à fila!",
-            description = data[1],
+            title = f"Música adicionada à fila!",
+            description = f"A pedido de <@{ctx.author.id}>, teremos {data[1]}",
         )
         
         embed.set_thumbnail(url=data[2])
@@ -125,7 +128,7 @@ async def next(ctx):
 
 @client.command()
 async def comandos(ctx):
-    if ctx.channel.id == 864553084956049418:
+    if ctx.channel.id == 1244791679257415691:
         await ctx.send("Lista de comandos: \n!play <NomeDaMusica ou URL> -> Reproduz a música escolhida ou adiciona na fila se uma já estiver tocando" +
                     "\n\n!pause -> Pausa a música que está tocando \n\n!unpause -> Despausa a música \n\n!stop -> Para de reproduzir áudio \n\n!next -> Passa para a próxima música" +
                     "\n\n!leave -> Faz o bot sair do canal de voz")
@@ -133,6 +136,22 @@ async def comandos(ctx):
 @tasks.loop(seconds=10)
 async def updateQueue():
     await nextSong()
+
+@tasks.loop(seconds=60)
+async def timeoutLeave():
+    global timedOut, musicQueue, musicChannel
+
+    voice = discord.utils.get(client.voice_clients)
+    if timedOut and musicChannel != None: # Se tava preparado pra sair e a lista ta vazia
+        await musicChannel.voice_client.disconnect()
+        musicChannel = None
+    
+    if voice != None:
+        if musicQueue == [] and not voice.is_playing(): # Se a lista estiver vazia e não estiver tocando nada
+            timedOut = True # Se prepara pra sair
+
+
+    
 
 #Escondendo o token
 f = open("C:/Users/enriq/Documents/auth_disc.txt", "r")
